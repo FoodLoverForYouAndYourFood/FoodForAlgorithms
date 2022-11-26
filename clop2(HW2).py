03b5c1d700074b327c8faa71f8bc4e21710bc45a
David <112866781+FoodLoverForYouAndYourFood@users.noreply.github.com>
2022-11-26 21:25:55 +0300

    Update clop2(HW2).py

--- clop2(HW2).py
+++ clop2(HW2).py
@@ -1,14 +1,14 @@
-""" Сложность алгоритма O(n) из-за одного цикла"""
+""" Сложность алгоритма O(n) из-за одного цикла. Заполняем так же как показано в объяснении. Выводим максимальное"""
 
 def MAXgEN(n):
     nums = [0] * (n + 1)
     nums[1] = 1
-    for i in range(1, len(nums)): #заполняем так же как показано в объяснении
+    for i in range(1, len(nums)): 
         if 2 <= 2 * i <= n:
             nums[2 * i] = nums[i]
         if 2 <= 2 * i + 1 <= n:
             nums[2 * i + 1] = nums[i] + nums[i + 1]
-    return max(nums) # выводим максимальное
+    return max(nums) 
 
 
 print(MAXgEN(3))
