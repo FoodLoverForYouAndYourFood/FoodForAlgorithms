132728c41eb613557cca7368196b93c0bb121345
David <112866781+FoodLoverForYouAndYourFood@users.noreply.github.com>
2022-12-14 11:50:09 +0300

    Update clop5.py

--- clop5.py
+++ clop5.py
@@ -1,1 +1,16 @@
-# in progress
+def removeOuterParentheses(s: str) -> str:
+    stack, stack2 = [], []
+    for i in s:
+        if i == '(':  # проверяем явлется ли скобка открыващей для добавления в стек
+            stack.append(i)
+            if len(stack) > 1:
+                stack2.append(i)
+        elif i == ')' and len(stack) != 1:
+            stack2.append(i)
+            stack.pop()
+        else:
+            stack.clear()
+    return ''.join(stack2)
+
+
+print(f"'{removeOuterParentheses(input())}'")
